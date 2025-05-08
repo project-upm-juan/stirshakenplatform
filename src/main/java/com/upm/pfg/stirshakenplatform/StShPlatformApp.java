a38478b771cc4e1886da9614cea3cb1d78abdf5b
juans <js.cardona@alumnos.upm.es>
2025-05-08 15:52:05 +0200

    First commit

---
+++ src/main/java/com/upm/pfg/stirshakenplatform/StShPlatformApp.java
@@ -0,0 +1,13 @@
+package com.upm.pfg.stirshakenplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StShPlatformApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StShPlatformApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/upm/pfg/stirshakenplatform/StirshakenplatformApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.upm.pfg.stirshakenplatform;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class StirshakenplatformApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
